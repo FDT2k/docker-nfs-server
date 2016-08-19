56c9a3a1e45e98786a2164d487981e1f8ef8f13e
Fabien Di Tore <fabien@ditore.ch>
2016-08-19 11:49:29 +0200

    add rpcbinds

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-
 set -e
 
 mounts="${@}"
+HOSTS="${HOSTS:-*}"
+PARAMS="${PARAMS:-(rw,sync,no_root_squash,no_subtree_check,insecure,fsid=0)}"
+
+echo "nfs        2049/tcp   # Network File System" >> /etc/services
+echo "nfs        2049/udp   # Network File System" >> /etc/services
+echo "sunrpc     111/tcp    portmapper   # RPC 4.0 portmapper" >> /etc/services
+echo "sunrpc     111/udp    portmapper" >> /etc/services
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-
 	if [ ! -d $src ]; then
 			mkdir -p $src
 	fi
-
-	echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
-
+  echo "$src $HOSTS$PARAMS" >> /etc/exports
+  echo "Added: $src $HOSTS$PARAMS"
 done
 
 exec runsvdir /etc/sv
